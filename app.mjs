a64e1f3bd4c59c39203c8c1cea62e7e9459b890b
Abdul Karim <abdulkarimmandvia76@gmail.com>
2024-05-12 02:35:09 +0500

    Q1

---
+++ app.mjs
@@ -0,0 +1,13 @@
+//Q:1
+// Integer 
+let integer = parseFloat(prompt("Enter  Positive Integer :"));
+console.log(`Number: ${integer}`);
+
+let roundOffNumber =Math.round(integer)
+console.log(`Round Off Number: ${roundOffNumber}`);
+
+let floorNumber = Math.floor(integer)
+console.log(`Floor Number: ${floorNumber}`);
+
+let ceilNumber = Math.ceil(integer)
+console.log(`Round Off Number: ${ceilNumber}`);
